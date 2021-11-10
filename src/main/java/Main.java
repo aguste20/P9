0213c1503f6f1cbd30b5cda405121c7257fea0c8
aguste20 <aguste20@student.aau.dk>
2021-11-10 13:44:08 +0100

    Lolz

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    /**
+     * Main method, launches the application
+     * @param args command line arguments
+     */
+    public static void main(String[] args){
+        launch();
+    }
+
+
+    /**
+     * Implements abstract method inherited from Application
+     * Starts the application. Sets stage, scene and root to hold contents.
+     * @param stage primary stage provided by this.launch()
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
