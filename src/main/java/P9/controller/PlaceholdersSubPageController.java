b8633090d72169579a49269ab282a4482700ea98
Mads <madssighlund@live.dk>
2021-11-18 10:41:41 +0100

    Initial code

--- src/main/java/P9/controller/PlaceholdersSubPageController.java
+++ src/main/java/P9/controller/PlaceholdersSubPageController.java
@@ -1,6 +1,22 @@
 package P9.controller;
 
-public class PlaceholdersSubPageController {
+import P9.Main;
+import P9.model.EObject;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PlaceholdersSubPageController implements Initializable {
     //TODO Lav en liste over placeholders
+
+    EObject eObject = new EObject();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        eObject = Main.getMainPageController().geteObject();
+    }
+
+
 
 }
