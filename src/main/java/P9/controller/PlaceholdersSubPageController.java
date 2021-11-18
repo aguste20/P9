367e0bf1171f4505db8e47e5861246f96fd863ab
Mads <madssighlund@live.dk>
2021-11-18 15:50:56 +0100

    Placeholder functionality added

--- src/main/java/P9/controller/PlaceholdersSubPageController.java
+++ src/main/java/P9/controller/PlaceholdersSubPageController.java
@@ -2,21 +2,80 @@ package P9.controller;
 
 import P9.Main;
 import P9.model.EObject;
+import P9.persistence.Setup;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class PlaceholdersSubPageController implements Initializable {
+    public Label objName;
+    public Label objVersion;
+    public Label objLength;
+    public Label objHeight;
+    public Label objWidth;
+    public Label objWeight;
+
+
     //TODO Lav en liste over placeholders
 
     EObject eObject = new EObject();
 
+    ArrayList<String> columns = new ArrayList<String>();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         eObject = Main.getMainPageController().geteObject();
+        setLabels();
+    }
+
+    public void setLabels(){
+        objName.setText(eObject.getName());
+        objVersion.setText(eObject.getVersion().toString());
+        objLength.setText(eObject.getLength().toString());
+        objHeight.setText(eObject.getHeight().toString());
+        objWidth.setText(eObject.getWidth().toString());
+        objWeight.setText(eObject.getWeight().toString());
     }
 
 
+    public void insertPlaceholder(ActionEvent e) {
+        String choice = ((Button) e.getSource()).getId();
+        TextArea text = Main.getTextEditorController().getTextArea();
+        int pos = Main.getTextEditorController().getTextArea().getCaretPosition();
 
+        switch (choice){
+            case "name":
+                text.insertText(pos, "</xsl:text><xsl:value-of select=\"name\"/><xsl:text>");
+        }
+        switch (choice){
+            case "version":
+                text.insertText(pos, "</xsl:text><xsl:value-of select=\"version\"/><xsl:text>");
+        }
+        switch (choice){
+            case "length":
+                text.insertText(pos, "</xsl:text><xsl:value-of select=\"length\"/><xsl:text>");
+        }
+        switch (choice){
+            case "height":
+                text.insertText(pos, "</xsl:text><xsl:value-of select=\"height\"/><xsl:text>");
+        }
+        switch (choice){
+            case "width":
+                text.insertText(pos, "</xsl:text><xsl:value-of select=\"width\"/><xsl:text>");
+        }
+        switch (choice){
+            case "weight":
+                text.insertText(pos, "</xsl:text><xsl:value-of select=\"weight\"/><xsl:text>");
+        }
+    }
 }
