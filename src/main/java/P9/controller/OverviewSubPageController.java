c6512394f3f56ecf65919bccf322ff0d3d639dd1
aguste20 <aguste20@student.aau.dk>
2021-11-24 15:02:23 +0100

    Refactoring: text

--- src/main/java/P9/controller/OverviewSubPageController.java
+++ src/main/java/P9/controller/OverviewSubPageController.java
@@ -6,46 +6,49 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
-import org.apache.commons.lang3.StringUtils;
 
 import java.net.URL;
 import java.util.*;
 
 public class OverviewSubPageController implements Initializable {
 
-    private EObjectDoc doc;
     private List<Header> headerList = new ArrayList<>();
-    private String theText;
+    private String text; // Text from the textarea
 
     @FXML
     private TreeView<String> tocView;
 
-    //TODO kommentarer
+
+    /**
+     * Method that initializes a contoller object after its root element has been loaded.
+     * Inherited from Initializable.
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // Get reference to displayed doc
-        doc = Main.getMainPageController().geteObject().getDoc();
-        theText = doc.getXmlText();
-
-        createDummyRoot();
+        createTocTreeViewRoot();
 
         // Create table of contents
         updateToc();
     }
 
 
-    //TODO kommentarer
+    /**
+     * Method that updates the table of contents
+     */
     public void updateToc() {
-        // Get text from doc from eObject
-        String txt = doc.getXmlText();
+        text = Main.getMainPageController().geteObject().getDoc().getXmlText();
 
         // Find indexes of all h1s
-        findIndexesOfAllh1(txt);
+        findIndexesOfAllh1();
     }
 
-
-
-    public void findIndexesOfAllh1(String text){
+    /**
+     * Method that finds all h1, adds them to table of content, finds h2 and adds those to toc
+     */
+    public void findIndexesOfAllh1(){
         String hTag = "<h1>";
         String hClosingTag = "</h1>";
 
@@ -72,9 +75,9 @@ public class OverviewSubPageController implements Initializable {
                 h.endIndex = text.length();
             }
 
-            System.out.println("Header at range: " + h.startIndex + " - " + h.endTagIndex + ". Headertext: " + h.getHeaderText(text) + " ends at index: " + h.endIndex);
+            System.out.println("Header at range: " + h.startIndex + " - " + h.endTagIndex + ". Headertext: " + h.getHeaderText() + " ends at index: " + h.endIndex);
 
-            addHeaderToTreeView(h, text);
+            addHeaderToTreeView(h);
 
             h.findAllH2(text);
 
@@ -82,18 +85,30 @@ public class OverviewSubPageController implements Initializable {
 
     }
 
-    public void addHeaderToTreeView(Header h, String text){
-        h.setTreeItem(new TreeItem<>(h.getHeaderText(text)));
-
+    /**
+     * Method that adds a header to the toc tree view element
+     * @param h header to be added
+     */
+    public void addHeaderToTreeView(Header h){
+        h.setTreeItem(new TreeItem<>(h.getHeaderText()));
         tocView.getRoot().getChildren().add(h.treeItem);
     }
 
+    /**
+     * Method that creates a root for the toc tree view
+     */
+    public void createTocTreeViewRoot(){
+        TreeItem<String> contents = new TreeItem<>("Contents");
+        tocView.setRoot(contents);
+        contents.setExpanded(true);
+    }
 
 
-
-    //TODO kommentarer
+    /**
+     * Inner class that represents a header
+     * Used to store header index position and treeItem in treeView
+     */
     private class Header {
-        String txt;
         Integer startIndex; //Index of the first character in "<h1>"
         Integer endTagIndex; //Index of the first character in "</h1>"
         Integer endIndex; // Index of the next occurrence of "<h1>". Is set to final index position of text, if no more h1's
@@ -106,8 +121,7 @@ public class OverviewSubPageController implements Initializable {
             this.endTagIndex = endTagIndex;
         }
 
-        public Header(String txt, Integer startIndex, Integer endTagIndex, Integer type) {
-            this.txt = txt;
+        public Header(Integer startIndex, Integer endTagIndex, Integer type) {
             this.startIndex = startIndex;
             this.endTagIndex = endTagIndex;
             this.type = type;
@@ -121,7 +135,7 @@ public class OverviewSubPageController implements Initializable {
             this.treeItem = treeItem;
         }
 
-        public String getHeaderText(String text){
+        public String getHeaderText(){
             String htext = text.substring(startIndex, endTagIndex);
 
             String h = htext.replace("<h1>", "");
@@ -144,7 +158,7 @@ public class OverviewSubPageController implements Initializable {
                 index = text.indexOf(hTag, index + hTag.length());
                 endIndex = text.indexOf(hClosingTag, index);
 
-                System.out.println("Header at range: " + h2.startIndex + " - " + h2.endTagIndex + ". Headertext: " + h2.getHeaderText(text) + " ends at index: " + h2.endIndex);
+                System.out.println("Header at range: " + h2.startIndex + " - " + h2.endTagIndex + ". Headertext: " + h2.getHeaderText() + " ends at index: " + h2.endIndex);
 
                 //Ad to treeview
                 this.addH2ToTreeView(h2);
@@ -153,172 +167,11 @@ public class OverviewSubPageController implements Initializable {
         }
 
         public void addH2ToTreeView(Header h2){
-            TreeItem<String> item = new TreeItem<>(h2.getHeaderText(theText));
-
+            TreeItem<String> item = new TreeItem<>(h2.getHeaderText());
             treeItem.getChildren().add(item);
         }
     }
 
-    public void createDummyRoot(){
-        TreeItem<String> contents = new TreeItem<>("Contents");
-        tocView.setRoot(contents);
-        contents.setExpanded(true);
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // ------------------- skrald -----------------
-
-     /*
-    //TODO kommentarer
-    public void addHeaderToAccordion(String header){
-        // Create titled pane from found heading string
-        TitledPane tp = new TitledPane();
-        tp.setText(header);
-
-        // Append new titled pane to accordion in scene
-        tocAccordion.getPanes().add(tp);
-
-    }
-
-     */
-
-    //TODO kommentarer
-    public List<String> findHeaders(String txt, int type) {
-
-        ArrayList<String> headerList = new ArrayList<>();
-
-        // Create tag strings to look for
-        String hTag = ("<h" + type + ">");
-        String hClosingTag = ("</h" + type + ">");
-
-        // Parse string for substrings
-        if (txt.contains(hTag)) {
-
-            // String to be returned by the method
-            String headerString = "";
-
-            int j = 0;
-
-            int firstIndex;
-            int lastIndex;
-
-            // Iterate over text, looking for headers
-            for (int i = 0; i < txt.length(); i += j) {
-                // Find index of the header string, looking from index j
-                firstIndex = txt.indexOf(hTag, j);
-                lastIndex = txt.indexOf(hClosingTag, j);
-
-                // If no header found, break for loop
-                if (firstIndex == -1) {
-                    break;
-                }
-
-                // Get substring at specified index
-                headerString = txt.substring(firstIndex + 4, lastIndex);
-
-                headerList.add(headerString);
-
-                // Increment j (index position to look from)
-                j = lastIndex + 5;
-            }
-
-        }
-        return headerList;
-    }
-
-    public List<Header> findAllH1(String txt){
-
-        // List to return
-        ArrayList<Header> headerList = new ArrayList<>();
-
-        // Tags to look for
-        String hTag = "<h1>";
-        String hClosingTag = "</h1>";
-
-        // Look for all h1's and save in list
-        String[] h1s = StringUtils.substringsBetween(txt, hTag, hClosingTag);
-
-        // Iterate over all h1 strings and save as header object,
-        // starting from index 0 and incrementing as a new header is found
-        int lookFrom = 0;
-        for (String s : h1s){
-            String match = "<h1>" + s + "</h1>";
-
-            int matchIndex = txt.indexOf(s) - 4;
-
-            match.equals(s);
-
-            int index = txt.indexOf(s, lookFrom);
-
-            Header h = new Header(s, index, (index + s.length()), 1);
-
-            headerList.add(h);
-
-            System.out.println("H1: " + h.txt + " at index: " + h.startIndex + " and end index: " + h.endTagIndex);
-
-            lookFrom = index + s.length();
-        }
-
-
-
-
-
-
-        return headerList;
-
-    }
-
-
-
-            /*
-                //Parse text for headings with iterator, save in list
-            CharacterIterator it = new StringCharacterIterator(txt);
-
-            while (it.current() != CharacterIterator.DONE)
-            {
-                System.out.print(it.current());
-
-
-                it.next();
-            }
-             */
-
-
-    // Find all h1 headers
-    //findAllH1(txt);
-
-        /*
-        List<String> headerList = findHeaders(txt, 1);
-
-        for(String headerString : headerList){
-            //addHeaderToAccordion(headerString);
-
-            //TODO Anne: Find h2 for denne h1
-            //TODO Anne: Tilføj accordion til h1-tp + tilføj h2-tp til accordion
-
-            TitledPane tp2 = new TitledPane();
-            tp2.setText("h2");
-
-            Accordion h1Acc = new Accordion();
-            h1Acc.getPanes().add(tp2);
-
-            tp.setContent(h1Acc);
-            */
 
 
 }
